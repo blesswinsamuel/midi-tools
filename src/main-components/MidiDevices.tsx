2080bb30bdea860c4870cf80ca2f71c5533b0460
Blesswin Samuel <blesswinsamuel@gmail.com>
2022-05-28 00:02:47 +0530

    Midi devices enhancements

--- src/main-components/MidiDevices.tsx
+++ src/main-components/MidiDevices.tsx
@@ -1,31 +1,32 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { PolySynth, ToneAudioNode } from 'tone'
 import { Input, Output } from 'webmidi'
-import { HTMLTable, Tab, Tabs } from '@blueprintjs/core'
+import { Button, ButtonGroup, Divider, HTMLTable, Tab, Tabs } from '@blueprintjs/core'
 import { useWebMidiDevices } from '../components/WebMidi'
+import useLocalStorageState from '../components/hooks/useLocalStorageState'
 
 function DeviceTable({ devices }: { devices: (Input | Output)[] }) {
   return (
     <HTMLTable condensed bordered>
       <thead>
         <tr>
-          {/* <th>Connection</th> */}
+          <th>Connection</th>
           <th>ID</th>
           <th>Manufacturer</th>
           <th>Name</th>
-          {/* <th>State</th> */}
-          {/* <th>Type</th> */}
+          <th>State</th>
+          <th>Type</th>
         </tr>
       </thead>
       <tbody>
         {devices.map((input) => (
           <tr key={input.id}>
-            {/* <td>{input.connection}</td> */}
+            <td>{input.connection}</td>
             <td>{input.id}</td>
             <td>{input.manufacturer}</td>
             <td>{input.name}</td>
-            {/* <td>{input.state}</td> */}
-            {/* <td>{input.type}</td> */}
+            <td>{input.state}</td>
+            <td>{input.type}</td>
           </tr>
         ))}
       </tbody>
@@ -34,17 +35,32 @@ function DeviceTable({ devices }: { devices: (Input | Output)[] }) {
 }
 
 export default function MidiDevices() {
+  const [showInputs, setShowInputs] = useLocalStorageState('midi-devices:inputs', true)
+  const [showOutputs, setShowOutputs] = useLocalStorageState('midi-devices:outputs', true)
   const synth = useRef<ToneAudioNode | null>(null)
   if (synth.current === null) {
     synth.current = new PolySynth().toDestination()
   }
 
   const { inputs, outputs } = useWebMidiDevices()
-
+  const devices = [...inputs, ...outputs]
+  const filteredDevices = devices.filter((device) => {
+    if (!showInputs && !showOutputs) return true
+    if (device.type === 'input' && showInputs) return true
+    if (device.type === 'output' && showOutputs) return true
+  })
   return (
-    <Tabs id="midi-devices">
-      <Tab id="in" title="Inputs" panel={<DeviceTable devices={inputs} />} />
-      <Tab id="out" title="Outputs" panel={<DeviceTable devices={outputs} />} />
-    </Tabs>
+    <div>
+      <ButtonGroup>
+        <Button onClick={() => setShowInputs((v) => !v)} active={showInputs}>
+          Inputs
+        </Button>
+        <Button onClick={() => setShowOutputs((v) => !v)} active={showOutputs}>
+          Outputs
+        </Button>
+      </ButtonGroup>
+      <p></p>
+      <DeviceTable devices={filteredDevices} />
+    </div>
   )
 }
