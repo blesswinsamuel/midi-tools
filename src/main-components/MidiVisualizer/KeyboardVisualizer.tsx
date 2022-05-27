fde081b9717db1049f775c41e335a83774ef9269
Blesswin Samuel <blesswinsamuel@gmail.com>
2022-05-28 00:03:25 +0530

    Midi keyboard visualizer improvements

--- src/main-components/MidiVisualizer/KeyboardVisualizer.tsx
+++ src/main-components/MidiVisualizer/KeyboardVisualizer.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Input, NoteMessageEvent } from 'webmidi'
+import { Input, NoteMessageEvent, MessageEvent, ControlChangeMessageEvent } from 'webmidi'
 import { classNames } from '../../components/classNames'
-import useLocalStorageState from '../../components/hooks/useLocalStorageState'
-import MidiDeviceSelector from '../../components/MidiDeviceSelector'
-import { useWebMidiDevice } from '../../components/WebMidi'
 
 interface IPianoKeyProps {
   note: string
@@ -17,7 +14,7 @@ function PianoKey({ hotkey, note, pressed }: IPianoKeyProps) {
   const isPressed = pressed
   return (
     <div
-      className={classNames('h-28 relative', {
+      className={classNames('h-14 relative', {
         'z-0 flex-1': isWhite,
         'z-10': isBlack,
       })}
@@ -45,11 +42,38 @@ function PianoKey({ hotkey, note, pressed }: IPianoKeyProps) {
             'text-[0.3rem]': isBlack,
           })}
         >
-          <span className="opacity-50">{note}</span>
+          {/* <span className="opacity-50">{note}</span>
           <br />
-          <kbd>{hotkey}</kbd>
+          <kbd>{hotkey}</kbd> */}
         </div>
       </div>
+    </div>
+  )
+}
+
+function SustainPedal({ pressed }: { pressed: boolean }) {
+  return (
+    <div className="flex justify-center">
+      <div className={classNames(pressed ? 'bg-gray-700' : 'bg-gray-900', 'w-10', 'h-10')}></div>
+    </div>
+  )
+}
+
+function PitchBend({ value }: { value: number }) {
+  return (
+    <div className="bg-gray-900 w-4 h-14 relative">
+      <div
+        className={classNames('bg-gray-700 w-full', 'absolute')}
+        style={{ top: (1 - Math.max(value, 0)) * 50 + '%', bottom: (1 + Math.min(value, 0)) * 50 + '%' }}
+      ></div>
+    </div>
+  )
+}
+
+function ModWheel({ value }: { value: number }) {
+  return (
+    <div className="bg-gray-900 w-4 h-14 relative">
+      <div className={classNames('bg-gray-700 w-full', 'absolute', 'bottom-0')} style={{ height: value * 100 + '%' }}></div>
     </div>
   )
 }
@@ -57,18 +81,51 @@ function PianoKey({ hotkey, note, pressed }: IPianoKeyProps) {
 const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 const octaves = [0, 1, 2, 3, 4, 5, 6, 7]
 
+function PianoKeys({ keys }: { keys: { [key: number]: boolean } }) {
+  return (
+    <div className="flex flex-1">
+      {octaves.flatMap((octave) =>
+        noteNames.map((noteName, noteIdx) => (
+          <PianoKey key={`${noteName}${octave}`} note={`${noteName}${octave}`} pressed={keys[11 + 12 * octave + (noteIdx + 1)]} />
+        ))
+      )}
+    </div>
+  )
+}
+
 export default function KeyboardVisualizer({ input }: { input: Input }) {
   const [keys, setKeys] = useState<{ [key: number]: boolean }>(() => ({}))
+  const [pitchBend, setPitchBend] = useState(0)
+  const [modWheel, setModWheel] = useState(0)
+  const [sustainPedal, setSustainPedal] = useState(false)
   useEffect(() => {
     if (!input) return
     const noteOn = (e: NoteMessageEvent) => setKeys((keys) => Object.assign({}, keys, { [e.note.number]: true }))
     const noteOff = (e: NoteMessageEvent) => setKeys((keys) => Object.assign({}, keys, { [e.note.number]: false }))
+    const pitchBend = (e: MessageEvent) => setPitchBend((e.rawValue || 0) / 8192 - 1)
+    const modWheel = (e: ControlChangeMessageEvent) => setModWheel((e.rawValue || 0) / 127)
+    const sustainPedal = (e: ControlChangeMessageEvent) => setSustainPedal(e.rawValue ? true : false)
+    const controlChange = (e: ControlChangeMessageEvent) => {
+      if (e.controller.number === 1) modWheel(e)
+      else if (e.controller.number === 64) sustainPedal(e)
+    }
+    // const controlChangeDebug = (e: ControlChangeMessageEvent) => console.log(e)
     const options = { channels: undefined }
     input.addListener('noteon', noteOn, options)
     input.addListener('noteoff', noteOff, options)
+    input.addListener('pitchbend', pitchBend, options)
+    input.addListener('controlchange', controlChange, options)
+    // input.addListener('controlchange', controlChangeDebug, options)
+    // input.addListener('controlchange-controller1', modWheel, options)
+    // input.addListener('controlchange-controller64', sustainPedal, options)
     return () => {
       input.removeListener('noteon', noteOn, options)
       input.removeListener('noteoff', noteOff, options)
+      input.removeListener('pitchbend', pitchBend, options)
+      input.removeListener('controlchange', controlChange, options)
+      // input.removeListener('controlchange', controlChangeDebug, options)
+      // input.removeListener('controlchange-controller1', modWheel, options)
+      // input.removeListener('controlchange-controller64', sustainPedal, options)
     }
   }, [input])
 
@@ -77,12 +134,13 @@ export default function KeyboardVisualizer({ input }: { input: Input }) {
   // }, [])
 
   return (
-    <div className="flex">
-      {octaves.flatMap((octave) =>
-        noteNames.map((noteName, noteIdx) => (
-          <PianoKey key={`${noteName}${octave}`} note={`${noteName}${octave}`} pressed={keys[11 + 12 * octave + (noteIdx + 1)]} />
-        ))
-      )}
+    <div className="flex flex-col gap-3">
+      <div className="flex gap-1">
+        <PitchBend value={pitchBend} />
+        <ModWheel value={modWheel} />
+        <PianoKeys keys={keys} />
+      </div>
+      <SustainPedal pressed={sustainPedal} />
     </div>
   )
 }
